2772a847d27c265b01cf33651530b788d302517c
MICHU <michalsloma1@gmail.com>
2026-03-12 17:59:16 +0100

    task3.cpp

--- tasks/task3.cpp
+++ tasks/task3.cpp
@@ -8,26 +8,3 @@
 //
 // Text used for testing:
 // "If you need help you can email support_team@example.com or cntact the administrator at admin123@server.com. Some old contacts such as help-desk@company and marketing@mail are no longer valid.@domain."
-#include <iostream>
-#include <regex>
-#include <string>
-
-int main() {
-
-    std::string text = "If you need help you can email support_team@example.com or cntact the administrator at admin123@server.com. Some old contacts such as help-desk@company and marketing@mail are no longer valid.@domain.";
-
-    std::regex pattern(R"(\w+@[a-z]+\.com)");
-
-    std::sregex_iterator it(text.begin(), text.end(), pattern);
-    std::sregex_iterator end;
-
-    if (it == end) {
-        std::cout << "No valid emails found" << std::endl;
-    } else {
-        for (; it != end; ++it) {
-            std::cout << "Valid email found: " << it->str() << std::endl;
-        }
-    }
-
-    return 0;
-}
